d136ce054c99341221eff07c8468b59ca2c8123b
imnotonehanded <59941757+imnotonehanded@users.noreply.github.com>
2020-07-11 15:56:21 -0500

    Update and rename Skiddersgetskidded.txt to Skiddersgetskidded.lua

---
+++ Skiddersgetskidded.lua
@@ -0,0 +1,405 @@
+-- Gui to Lua
+-- Version: 3.2
+
+-- Instances:
+
+local MiztHub = Instance.new("ScreenGui")
+local MainFrame = Instance.new("ImageLabel")
+local n5 = Instance.new("TextButton")
+local FlingGun = Instance.new("TextButton")
+local Name = Instance.new("TextLabel")
+local Sonic = Instance.new("TextButton")
+local Joy = Instance.new("TextButton")
+local ElioBlasio = Instance.new("TextButton")
+local Ender = Instance.new("TextButton")
+local Killbotv2 = Instance.new("TextButton")
+local CatV2 = Instance.new("TextButton")
+local Chill = Instance.new("TextButton")
+local Lutrisv2 = Instance.new("TextButton")
+local Memeusv25 = Instance.new("TextButton")
+local Memeanimation = Instance.new("TextButton")
+local Pen = Instance.new("TextButton")
+local Xester = Instance.new("TextButton")
+local Krystaldance = Instance.new("TextButton")
+local Credits = Instance.new("TextLabel")
+
+--Properties:
+
+MiztHub.Name = "MiztHub"
+MiztHub.Parent = game.CoreGui
+
+MainFrame.Name = "MainFrame"
+MainFrame.Parent = MiztHub
+MainFrame.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+MainFrame.BackgroundTransparency = 1.000
+MainFrame.Position = UDim2.new(0.60615623, 0, 0.39607203, 0)
+MainFrame.Size = UDim2.new(0, 412, 0, 242)
+MainFrame.Image = "rbxassetid://3570695787"
+MainFrame.ImageColor3 = Color3.fromRGB(11, 149, 255)
+MainFrame.ScaleType = Enum.ScaleType.Slice
+MainFrame.SliceCenter = Rect.new(100, 100, 100, 100)
+MainFrame.SliceScale = 0.120
+
+n5.Name = "n5"
+n5.Parent = MainFrame
+n5.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+n5.BorderSizePixel = 0
+n5.Position = UDim2.new(0.0145631135, 0, 0.236570239, 0)
+n5.Size = UDim2.new(0, 103, 0, 25)
+n5.Font = Enum.Font.GothamBold
+n5.Text = "n5"
+n5.TextColor3 = Color3.fromRGB(0, 85, 255)
+n5.TextSize = 14.000
+n5.TextWrapped = true
+
+FlingGun.Name = "Fling Gun"
+FlingGun.Parent = MainFrame
+FlingGun.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+FlingGun.BorderSizePixel = 0
+FlingGun.Position = UDim2.new(0.593886137, 0, 0.236570239, 0)
+FlingGun.Size = UDim2.new(0, 103, 0, 25)
+FlingGun.Font = Enum.Font.GothamBold
+FlingGun.Text = "Fling Gun"
+FlingGun.TextColor3 = Color3.fromRGB(0, 85, 255)
+FlingGun.TextSize = 14.000
+FlingGun.TextWrapped = true
+
+Name.Name = "Name"
+Name.Parent = MainFrame
+Name.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Name.BackgroundTransparency = 1.000
+Name.Position = UDim2.new(-0.152912632, 0, 0.02892562, 0)
+Name.Size = UDim2.new(0, 316, 0, 41)
+Name.Font = Enum.Font.GothamBold
+Name.Text = "Mizt Hub"
+Name.TextColor3 = Color3.fromRGB(0, 85, 255)
+Name.TextScaled = true
+Name.TextSize = 14.000
+Name.TextWrapped = true
+
+Sonic.Name = "Sonic"
+Sonic.Parent = MainFrame
+Sonic.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Sonic.BorderSizePixel = 0
+Sonic.Position = UDim2.new(0.295513123, 0, 0.236570239, 0)
+Sonic.Size = UDim2.new(0, 103, 0, 25)
+Sonic.Font = Enum.Font.GothamBold
+Sonic.Text = "Sonic"
+Sonic.TextColor3 = Color3.fromRGB(0, 85, 255)
+Sonic.TextSize = 14.000
+Sonic.TextWrapped = true
+
+Joy.Name = "Joy"
+Joy.Parent = MainFrame
+Joy.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Joy.BorderSizePixel = 0
+Joy.Position = UDim2.new(0.0145631135, 0, 0.384883881, 0)
+Joy.Size = UDim2.new(0, 103, 0, 25)
+Joy.Font = Enum.Font.GothamBold
+Joy.Text = "Joy"
+Joy.TextColor3 = Color3.fromRGB(0, 85, 255)
+Joy.TextSize = 14.000
+Joy.TextWrapped = true
+
+ElioBlasio.Name = "Elio Blasio"
+ElioBlasio.Parent = MainFrame
+ElioBlasio.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+ElioBlasio.BorderSizePixel = 0
+ElioBlasio.Position = UDim2.new(0.295513123, 0, 0.384883881, 0)
+ElioBlasio.Size = UDim2.new(0, 103, 0, 25)
+ElioBlasio.Font = Enum.Font.GothamBold
+ElioBlasio.Text = "Elio Blasio"
+ElioBlasio.TextColor3 = Color3.fromRGB(0, 85, 255)
+ElioBlasio.TextSize = 14.000
+ElioBlasio.TextWrapped = true
+
+Ender.Name = "Ender"
+Ender.Parent = MainFrame
+Ender.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Ender.BorderSizePixel = 0
+Ender.Position = UDim2.new(0.593886137, 0, 0.384883881, 0)
+Ender.Size = UDim2.new(0, 103, 0, 25)
+Ender.Font = Enum.Font.GothamBold
+Ender.Text = "Ender"
+Ender.TextColor3 = Color3.fromRGB(0, 85, 255)
+Ender.TextSize = 14.000
+Ender.TextWrapped = true
+
+Killbotv2.Name = "Killbotv2"
+Killbotv2.Parent = MainFrame
+Killbotv2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Killbotv2.BorderSizePixel = 0
+Killbotv2.Position = UDim2.new(0.0145631135, 0, 0.529489517, 0)
+Killbotv2.Size = UDim2.new(0, 103, 0, 25)
+Killbotv2.Font = Enum.Font.GothamBold
+Killbotv2.Text = "Killbot V2"
+Killbotv2.TextColor3 = Color3.fromRGB(0, 85, 255)
+Killbotv2.TextSize = 14.000
+Killbotv2.TextWrapped = true
+
+CatV2.Name = "Cat V2"
+CatV2.Parent = MainFrame
+CatV2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+CatV2.BorderSizePixel = 0
+CatV2.Position = UDim2.new(0.295513123, 0, 0.529489517, 0)
+CatV2.Size = UDim2.new(0, 103, 0, 25)
+CatV2.Font = Enum.Font.GothamBold
+CatV2.Text = "Cat V2"
+CatV2.TextColor3 = Color3.fromRGB(0, 85, 255)
+CatV2.TextSize = 14.000
+CatV2.TextWrapped = true
+
+Chill.Name = "Chill"
+Chill.Parent = MainFrame
+Chill.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Chill.BorderSizePixel = 0
+Chill.Position = UDim2.new(0.593886137, 0, 0.529489517, 0)
+Chill.Size = UDim2.new(0, 103, 0, 25)
+Chill.Font = Enum.Font.GothamBold
+Chill.Text = "Chill"
+Chill.TextColor3 = Color3.fromRGB(0, 85, 255)
+Chill.TextSize = 14.000
+Chill.TextWrapped = true
+
+Lutrisv2.Name = "Lutris v2"
+Lutrisv2.Parent = MainFrame
+Lutrisv2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Lutrisv2.BorderSizePixel = 0
+Lutrisv2.Position = UDim2.new(0.0145631135, 0, 0.672822177, 0)
+Lutrisv2.Size = UDim2.new(0, 103, 0, 25)
+Lutrisv2.Font = Enum.Font.GothamBold
+Lutrisv2.Text = "Lustris V2"
+Lutrisv2.TextColor3 = Color3.fromRGB(0, 85, 255)
+Lutrisv2.TextSize = 14.000
+Lutrisv2.TextWrapped = true
+
+Memeusv25.Name = "Memeus v25"
+Memeusv25.Parent = MainFrame
+Memeusv25.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Memeusv25.BorderSizePixel = 0
+Memeusv25.Position = UDim2.new(0.295513123, 0, 0.672822177, 0)
+Memeusv25.Size = UDim2.new(0, 103, 0, 25)
+Memeusv25.Font = Enum.Font.GothamBold
+Memeusv25.Text = "Memeus V2.5"
+Memeusv25.TextColor3 = Color3.fromRGB(0, 85, 255)
+Memeusv25.TextSize = 14.000
+Memeusv25.TextWrapped = true
+
+Memeanimation.Name = "Meme animation"
+Memeanimation.Parent = MainFrame
+Memeanimation.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Memeanimation.BorderSizePixel = 0
+Memeanimation.Position = UDim2.new(0.593886137, 0, 0.672822177, 0)
+Memeanimation.Size = UDim2.new(0, 103, 0, 25)
+Memeanimation.Font = Enum.Font.GothamBold
+Memeanimation.Text = "Meme Anim"
+Memeanimation.TextColor3 = Color3.fromRGB(0, 85, 255)
+Memeanimation.TextSize = 14.000
+Memeanimation.TextWrapped = true
+
+Pen.Name = "Pen"
+Pen.Parent = MainFrame
+Pen.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Pen.BorderSizePixel = 0
+Pen.Position = UDim2.new(0.295513123, 0, 0.818701088, 0)
+Pen.Size = UDim2.new(0, 103, 0, 25)
+Pen.Font = Enum.Font.GothamBold
+Pen.Text = "Pen"
+Pen.TextColor3 = Color3.fromRGB(0, 85, 255)
+Pen.TextSize = 14.000
+Pen.TextWrapped = true
+
+Xester.Name = "Xester"
+Xester.Parent = MainFrame
+Xester.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Xester.BorderSizePixel = 0
+Xester.Position = UDim2.new(0.593886137, 0, 0.818701088, 0)
+Xester.Size = UDim2.new(0, 103, 0, 25)
+Xester.Font = Enum.Font.GothamBold
+Xester.Text = "Xester"
+Xester.TextColor3 = Color3.fromRGB(0, 85, 255)
+Xester.TextSize = 14.000
+Xester.TextWrapped = true
+
+Krystaldance.Name = "Krystal dance"
+Krystaldance.Parent = MainFrame
+Krystaldance.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Krystaldance.BorderSizePixel = 0
+Krystaldance.Position = UDim2.new(0.0145631135, 0, 0.818701088, 0)
+Krystaldance.Size = UDim2.new(0, 103, 0, 25)
+Krystaldance.Font = Enum.Font.GothamBold
+Krystaldance.Text = "Krystal Dance"
+Krystaldance.TextColor3 = Color3.fromRGB(0, 85, 255)
+Krystaldance.TextSize = 14.000
+Krystaldance.TextWrapped = true
+
+Credits.Name = "Credits"
+Credits.Parent = MainFrame
+Credits.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Credits.BackgroundTransparency = 1.000
+Credits.Position = UDim2.new(0.451456308, 0, 0.123966932, 0)
+Credits.Size = UDim2.new(0, 127, 0, 18)
+Credits.Font = Enum.Font.GothamBold
+Credits.Text = "Credits: Mizt#5033"
+Credits.TextColor3 = Color3.fromRGB(0, 85, 255)
+Credits.TextSize = 14.000
+Credits.TextWrapped = true
+
+-- Scripts:
+
+local function ADVFFG_fake_script() -- n5.LocalScript 
+	local script = Instance.new('LocalScript', n5)
+
+	script.Parent.MouseButton1Click:Connect(function()
+		loadstring(game:HttpGetAsync("https://pastebin.com/raw/t0Mkc33N"))()
+	end)
+end
+coroutine.wrap(ADVFFG_fake_script)()
+local function IELGZR_fake_script() -- FlingGun.LocalScript 
+	local script = Instance.new('LocalScript', FlingGun)
+
+	script.Parent.MouseButton1Click:Connect(function()
+		loadstring(game:HttpGetAsync("https://pastebin.com/raw/0Jv3udmi"))()
+	end)
+end
+coroutine.wrap(IELGZR_fake_script)()
+local function YNHVUC_fake_script() -- Sonic.LocalScript 
+	local script = Instance.new('LocalScript', Sonic)
+
+	script.Parent.MouseButton1Click:Connect(function()
+		loadstring(game:HttpGetAsync("https://pastebin.com/raw/SyF5t70A"))()
+	end)
+end
+coroutine.wrap(YNHVUC_fake_script)()
+local function GYEKTD_fake_script() -- Joy.LocalScript 
+	local script = Instance.new('LocalScript', Joy)
+
+	script.Parent.MouseButton1Click:Connect(function()
+		loadstring(game:HttpGetAsync("https://pastebin.com/raw/0B76TD7N"))()
+	end)
+end
+coroutine.wrap(GYEKTD_fake_script)()
+local function UBORMX_fake_script() -- ElioBlasio.LocalScript 
+	local script = Instance.new('LocalScript', ElioBlasio)
+
+	script.Parent.MouseButton1Click:Connect(function()
+		loadstring(game:HttpGetAsync("https://pastebin.com/raw/xvBXu6Yc"))()
+	end)
+end
+coroutine.wrap(UBORMX_fake_script)()
+local function SASUZM_fake_script() -- Ender.LocalScript 
+	local script = Instance.new('LocalScript', Ender)
+
+	script.Parent.MouseButton1Click:Connect(function()
+		loadstring(game:HttpGetAsync("https://pastebin.com/raw/hv190Xiq"))()
+	end)
+end
+coroutine.wrap(SASUZM_fake_script)()
+local function FOMSMDY_fake_script() -- Killbotv2.LocalScript 
+	local script = Instance.new('LocalScript', Killbotv2)
+
+	script.Parent.MouseButton1Click:Connect(function()
+		loadstring(game:HttpGetAsync("https://pastebin.com/raw/G24tcRXA"))()
+	end)
+end
+coroutine.wrap(FOMSMDY_fake_script)()
+local function IGJYSWP_fake_script() -- CatV2.LocalScript 
+	local script = Instance.new('LocalScript', CatV2)
+
+	script.Parent.MouseButton1Click:Connect(function()
+		loadstring(game:HttpGetAsync("https://pastebin.com/raw/YLfR8Qx9"))()
+	end)
+end
+coroutine.wrap(IGJYSWP_fake_script)()
+local function UATFHDX_fake_script() -- Chill.LocalScript 
+	local script = Instance.new('LocalScript', Chill)
+
+	script.Parent.MouseButton1Click:Connect(function()
+		loadstring(game:HttpGetAsync("https://pastebin.com/raw/XmHFdTij"))()
+	end)
+end
+coroutine.wrap(UATFHDX_fake_script)()
+local function MPABCDO_fake_script() -- Lutrisv2.LocalScript 
+	local script = Instance.new('LocalScript', Lutrisv2)
+
+	script.Parent.MouseButton1Click:Connect(function()
+		loadstring(game:HttpGetAsync("https://pastebin.com/raw/6jeamwQL"))()
+	end)
+end
+coroutine.wrap(MPABCDO_fake_script)()
+local function CRFQHM_fake_script() -- Memeusv25.LocalScript 
+	local script = Instance.new('LocalScript', Memeusv25)
+
+	script.Parent.MouseButton1Click:Connect(function()
+		loadstring(game:HttpGetAsync("https://pastebin.com/raw/cCMJ9C82"))()
+	end)
+end
+coroutine.wrap(CRFQHM_fake_script)()
+local function APZO_fake_script() -- Memeanimation.LocalScript 
+	local script = Instance.new('LocalScript', Memeanimation)
+
+	script.Parent.MouseButton1Click:Connect(function()
+		loadstring(game:HttpGetAsync("https://pastebin.com/raw/0QfjMKrF"))()
+	end)
+end
+coroutine.wrap(APZO_fake_script)()
+local function VYRGX_fake_script() -- Pen.LocalScript 
+	local script = Instance.new('LocalScript', Pen)
+
+	script.Parent.MouseButton1Click:Connect(function()
+		loadstring(game:HttpGetAsync("https://pastebin.com/raw/1qcpeuDm"))()
+	end)
+end
+coroutine.wrap(VYRGX_fake_script)()
+local function IGWPG_fake_script() -- Krystaldance.LocalScript 
+	local script = Instance.new('LocalScript', Krystaldance)
+
+	script.Parent.MouseButton1Click:Connect(function()
+		loadstring(game:HttpGetAsync("https://pastebin.com/raw/TELjLdEU"))()
+	end)
+end
+coroutine.wrap(IGWPG_fake_script)()
+local function PMZVM_fake_script() -- MainFrame.SmoothDrag 
+	local script = Instance.new('LocalScript', MainFrame)
+
+	local Drag = script.Parent
+	gsCoreGui = game:GetService("CoreGui")
+	gsTween = game:GetService("TweenService")
+	local UserInputService = game:GetService("UserInputService")
+		local dragging
+		local dragInput
+		local dragStart
+		local startPos
+		local function update(input)
+			local delta = input.Position - dragStart
+			local dragTime = 0.04
+			local SmoothDrag = {}
+			SmoothDrag.Position = UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X, startPos.Y.Scale, startPos.Y.Offset + delta.Y)
+			local dragSmoothFunction = gsTween:Create(Drag, TweenInfo.new(dragTime, Enum.EasingStyle.Sine, Enum.EasingDirection.InOut), SmoothDrag)
+			dragSmoothFunction:Play()
+		end
+		Drag.InputBegan:Connect(function(input)
+			if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
+				dragging = true
+				dragStart = input.Position
+				startPos = Drag.Position
+				input.Changed:Connect(function()
+					if input.UserInputState == Enum.UserInputState.End then
+						dragging = false
+					end
+				end)
+			end
+		end)
+		Drag.InputChanged:Connect(function(input)
+			if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
+				dragInput = input
+			end
+		end)
+		UserInputService.InputChanged:Connect(function(input)
+			if input == dragInput and dragging and Drag.Size then
+				update(input)
+			end
+		end)
+	
+end
+coroutine.wrap(PMZVM_fake_script)()
